fdb2132f49ac34dcdebf6ad0fbd79102b0a4b124
Camille <camillealmendes@gmail.com>
2024-10-11 16:06:26 -0300

    q

--- obj/Debug/net8.0/GestaoDeResiduos.AssemblyInfo.cs
+++ obj/Debug/net8.0/GestaoDeResiduos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoDeResiduos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e86070a6b6d615b4e689a302f51b4be4287dd42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0749e0bed28410e4a602dd0a91b9e243d4fc4461")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoDeResiduos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoDeResiduos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
